59191d926a7e14a3e3fe284c7f76e4e26b39b349
王磊 <sysuleiw@163.com>
2019-07-08 16:46:23 +0800

    创建主工程

---
+++ ComponentStudyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ComponentStudyDemo
+//
+//  Created by 王磊 on 2019/7/8.
+//  Copyright © 2019 王磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ComponentStudyDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ComponentStudyDemo
+//
+//  Created by 王磊 on 2019/7/8.
+//  Copyright © 2019 王磊. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
